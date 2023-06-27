d3a0ee3a09fce0d0101ff5d258f2fc01aa1754df
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-06-27 18:34:20 +0600

    decoder added and send data body

--- index.js
+++ index.js
@@ -10,7 +10,9 @@
 
 const http = require('http');
 // app object = module scaffolding
-
+// url
+const url = require('url');
+const { StringDecoder } = require('node:string_decoder'); 
 const app = {};
 
 // configuration
@@ -27,7 +29,28 @@ app.createServer = () =>{
 }
 // handle Request Response
 app.handleReqRes = (req, res) =>{
-    res.end('Hello World');
+    // request handle
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, ' ')
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject =req.headers;
+    
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+    req.on('data', (buffer)=>{
+        realData += decoder.write(buffer); 
+    })
+
+    req.on('end', ()=>{
+        realData += decoder.end();
+        console.log(realData);
+        // response handle
+        res.end('Hello World');
+    })
+
 }
 
 // start the server
